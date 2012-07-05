78b6b2cfa6f711ba95cb50c68b0e151fc38a100b
Michael Snoyman <michael@snoyman.com>
2012-07-05 08:19:35 +0300

    hspec 1.2

--- install.hs
+++ install.hs
@@ -180,7 +180,7 @@ main = shelly $ verbosely $ do
   if repo == "persistent"
     then do
       echo "installing packages for the tests suites"
-      cabal_install ["HUnit","QuickCheck","file-location","hspec >= 1.1 && < 1.2"]
+      cabal_install ["HUnit","QuickCheck","file-location","hspec >= 1.2 && < 1.3"]
 
       -- install all persistent packages without tests
       install False pkgs
@@ -193,7 +193,7 @@ main = shelly $ verbosely $ do
 
     else do
       echo "installing packages for the tests suites"
-      cabal_install ["HUnit","QuickCheck","hspec >= 1.1 && < 1.2", "blaze-html >= 0.5 && < 0.6"]
+      cabal_install ["HUnit","QuickCheck","hspec >= 1.2 && < 1.3", "blaze-html >= 0.5 && < 0.6"]
 
       echo [lt|installing #{repo} packages|]
       install True pkgs
